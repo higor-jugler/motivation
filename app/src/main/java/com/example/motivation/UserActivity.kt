4e9d8b1d4e84ef59f61f0981bc16b747e9045450
Higor Jugler <higorjugler@hotmail.com>
2023-04-24 18:44:59 -0300

    Criado metodo para navegar entre os layout

--- app/src/main/java/com/example/motivation/UserActivity.kt
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -1,22 +1,36 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.motivation.databinding.ActivityMainBinding
+import com.example.motivation.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityUserBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // Hide support bar
+        supportActionBar?.hide()
+        binding.buttonSave.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-        Toast.makeText(this, "Test", Toast.LENGTH_LONG).show()
+    override fun onClick(view: View) {
+        handleSave()
+    }
+
+    private fun handleSave() {
+        val name = binding.editYourName.text.toString()
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            Toast.makeText(this, "Digite um nome válido", Toast.LENGTH_LONG).show()
+        }
     }
 }
