ed009d1d625db438dd9c808617c1cb9e3cc822c7
Higor Jugler <higorjugler@hotmail.com>
2023-04-24 20:06:19 -0300

    Adicionado constante para key do SharedPreferences

--- app/src/main/java/com/example/motivation/ui/UserActivity.kt
+++ app/src/main/java/com/example/motivation/ui/UserActivity.kt
@@ -1,11 +1,12 @@
-package com.example.motivation
+package com.example.motivation.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.example.motivation.databinding.ActivityMainBinding
+import com.example.motivation.infra.MotivationConstants
+import com.example.motivation.infra.SecurityPreferences
 import com.example.motivation.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity(), View.OnClickListener {
@@ -16,19 +17,30 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         // Hide support bar
         supportActionBar?.hide()
+
         binding.buttonSave.setOnClickListener(this)
+
+        checkName()
     }
 
     override fun onClick(view: View) {
         handleSave()
     }
 
+    private fun checkName() {
+        val name = SecurityPreferences(this).getString(MotivationConstants.KEY.USER_NAME)
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
     private fun handleSave() {
         val name = binding.editYourName.text.toString()
         if (name != "") {
-            SecurityPreferences(this).storeString("USER_NAME", name)
+            SecurityPreferences(this).storeString(MotivationConstants.KEY.USER_NAME, name)
             startActivity(Intent(this, MainActivity::class.java))
         } else {
             Toast.makeText(this, "Digite um nome válido", Toast.LENGTH_LONG).show()
